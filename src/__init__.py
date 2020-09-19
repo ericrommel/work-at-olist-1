92ed531b42590bfc0a42678addd5e2378893a4a6
Eric Dantas <ericrommel@gmail.com>
2020-09-19 15:42:36 +0200

    Set error handling

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 
 
 def create_app(test_config=None):
@@ -18,5 +18,30 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    # Error handling
+    @app.errorhandler(400)
+    def bad_request(e):
+        return jsonify(error=str(e)), 400
+
+    @app.errorhandler(401)
+    def unauthorized(e):
+        return jsonify(error=str(e)), 401
+
+    @app.errorhandler(403)
+    def forbidden(e):
+        return jsonify(error=str(e)), 403
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return jsonify(error=str(e)), 404
+
+    @app.errorhandler(405)
+    def not_logged(e):
+        return jsonify(error=str(e)), 405
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return jsonify(error=str(e)), 500
 
     return app
