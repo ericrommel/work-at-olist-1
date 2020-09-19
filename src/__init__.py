fc5e294d3ed9a8ccb6c88e2a6722c1d4eb6bfa47
Eric Dantas <ericrommel@gmail.com>
2020-09-19 15:54:06 +0200

    Set an internal logging

--- src/__init__.py
+++ src/__init__.py
@@ -2,18 +2,26 @@ import os
 
 from flask import Flask, jsonify
 
+from log import Log
+
+LOGGER = Log("work-at-olist").get_logger(logger_name="app")
+
 
 def create_app(test_config=None):
+    LOGGER.info("Initialize Flask app")
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="TeMpOrArY"
     )
 
     if test_config is None:
+        LOGGER.info("test-config is None. Get configs from config.py")
         app.config.from_pyfile("config.py", silent=True)
     else:
+        LOGGER.info(f"test-config is not None ({test_config}). Add configs from mapping")
         app.config.from_mapping(test_config)
 
+    LOGGER.info("Create 'instance' folder")
     try:
         os.makedirs(app.instance_path)
     except OSError:
@@ -22,26 +30,32 @@ def create_app(test_config=None):
     # Error handling
     @app.errorhandler(400)
     def bad_request(e):
+        LOGGER.error(e)
         return jsonify(error=str(e)), 400
 
     @app.errorhandler(401)
     def unauthorized(e):
+        LOGGER.error(e)
         return jsonify(error=str(e)), 401
 
     @app.errorhandler(403)
     def forbidden(e):
+        LOGGER.error(e)
         return jsonify(error=str(e)), 403
 
     @app.errorhandler(404)
     def page_not_found(e):
+        LOGGER.error(e)
         return jsonify(error=str(e)), 404
 
     @app.errorhandler(405)
     def not_logged(e):
+        LOGGER.error(e)
         return jsonify(error=str(e)), 405
 
     @app.errorhandler(500)
     def internal_server_error(e):
+        LOGGER.error(e)
         return jsonify(error=str(e)), 500
 
     return app
