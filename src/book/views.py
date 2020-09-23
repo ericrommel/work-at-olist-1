b0f6c5e097a1b8d8c1b246d68b7e4b1baa422836
Eric Dantas <ericrommel@gmail.com>
2020-09-23 23:48:33 +0200

    Adjust Book to use association table with Author. Postman test updated

--- src/book/views.py
+++ src/book/views.py
@@ -1,9 +1,12 @@
-from flask import abort, jsonify, request, url_for
+import json
+
+from flask import abort, jsonify, request, url_for, g
 from sqlalchemy.exc import OperationalError, SQLAlchemyError
+from sqlalchemy.orm import joinedload
 
 from . import book
 from .. import db, LOGGER
-from ..models import Book, books_schema, book_schema
+from ..models import AuthorBook, Book, books_schema, book_schema, Author
 
 
 def get_paginated_list(query_result, url: str, start: int, limit: int) -> dict:
@@ -39,7 +42,7 @@ def get_paginated_list(query_result, url: str, start: int, limit: int) -> dict:
         pages["next"] = url + "?start=%d&limit=%d" % (start_copy, limit)
 
     LOGGER.info("Extract result according to the bounds")
-    pages["results"] = query_result[(start - 1) : (start - 1 + limit)]
+    pages["results"] = query_result[(start - 1): (start - 1 + limit)]
     return pages
 
 
@@ -53,7 +56,7 @@ def list_books(page=1, per_page=20):
 
     LOGGER.info("Get the list of books from the database")
     try:
-        all_books = books_schema.dump(Book.query.order_by(Book.id.asc()))
+        all_books = books_schema.dump(Book.query.order_by(Book.name.asc()))
     except OperationalError:
         LOGGER.info("There is no book in the database")
         all_books = None
@@ -62,14 +65,12 @@ def list_books(page=1, per_page=20):
         return jsonify({"Warning": "There is no data to show"})
 
     LOGGER.info("Response the list of books")
-    return jsonify(
-        get_paginated_list(
-            query_result=all_books,
-            url=url_for("book.list_books"),
-            start=request.args.get("start", page),
-            limit=request.args.get("limit", per_page),
-        )
-    )
+    return get_paginated_list(
+        query_result=all_books,
+        url=url_for("book.list_books"),
+        start=request.args.get("start", page),
+        limit=request.args.get("limit", per_page),
+    ), 200
 
 
 @book.route("/books/<int:id>", methods=["GET"])
@@ -78,18 +79,17 @@ def books_detail(id):
     List details for a book
     """
 
-    book_instance = Book.query.get_or_404(id)
-    return book_schema.jsonify(book_instance)
+    book_instance = book_instance = Book.query.get_or_404(id)
+    author_instance = AuthorBook.query.filter_by(book_id=book_instance.id).all()
+    authors = [author.author_id for author in author_instance]
 
-
-@book.route("/books/<int:id>/authors", methods=["GET"])
-def list_authors_for_a_book(id):
-    """
-    List all authors for a book
-    """
-
-    book_instance = Book.query.get_or_404(id)
-    return books_schema.jsonify(book_instance.authors)
+    LOGGER.info(f"Return book added: '{book_instance.name}'")
+    return {
+        'id': book_instance.id,
+        'name': book_instance.name,
+        'publication_year': book_instance.publication_year,
+        'authors': authors
+    }, 200
 
 
 @book.route("/books/add", methods=["POST"])
@@ -98,26 +98,26 @@ def add_book():
     Add a book to the database
     """
 
+    request_fields = request.get_json() if request.get_json() else request.form
     LOGGER.info("Check for missed fields")
     mandatory_fields = ["name", "edition", "publication_year"]
     missing_fields = []
-    for i in mandatory_fields:
-        if i not in request.json:
-            LOGGER.info(f'{i} key is missing.')
-            missing_fields.append(i)
+    for m_field in mandatory_fields:
+        if m_field not in request_fields:
+            LOGGER.info(f'{m_field} key is missing.')
+            missing_fields.append(m_field)
     if missing_fields:
         abort(400, f"{' and '.join(missing_fields)} {'field is' if len(missing_fields) == 1 else 'fields are'} missing.")
 
     LOGGER.info("Set book variables from request")
     book_instance = Book()
-    book_instance.name = request.json.get("name")
-    book_instance.edition = request.json.get("edition")
-    book_instance.publication_year = request.json.get("publication_year")
-    book_instance.authors = request.json.get("authors", [])
+    book_instance.name = request_fields.get("name")
+    book_instance.edition = request_fields.get("edition")
+    book_instance.publication_year = request_fields.get("publication_year")
 
-    LOGGER.info(f"Add book {request.json.get('name')} to the database")
+    LOGGER.info(f"Add book '{book_instance.name}' to the database")
     try:
-        # Add book to the database
+        # Add book to the database (it will generate an book_id to store at the association table)
         db.session.add(book_instance)
         db.session.commit()
     except SQLAlchemyError as e:
@@ -126,7 +126,33 @@ def add_book():
     except Exception as e:
         abort(500, e)
 
-    return book_schema.jsonify(book_instance), 201
+    book_instance = Book.query.filter_by(name=request_fields.get("name")).first()
+    if 'authors' in request_fields:
+        LOGGER.info(f"Add authors for the book '{book_instance.name}'")
+
+        for author in request_fields.get("authors"):
+            association_instance = AuthorBook()
+            association_instance.author_id = author
+            association_instance.book_id = book_instance.id
+            try:
+                db.session.add(association_instance)
+                db.session.commit()
+            except SQLAlchemyError as e:
+                db.session.rollback()
+                abort(403, f"SQLAlchemyError: {e}")
+            except Exception as e:
+                abort(500, e)
+
+    LOGGER.info(f"Return book added: '{book_instance.name}'")
+    author_instance = AuthorBook.query.filter_by(book_id=book_instance.id).all()
+    authors = [author.author_id for author in author_instance]
+
+    return {
+        'id': book_instance.id,
+        'name': book_instance.name,
+        'publication_year': book_instance.publication_year,
+        'authors': authors
+    }, 201
 
 
 @book.route("/books/edit/<int:id>", methods=["PUT"])
@@ -135,18 +161,31 @@ def edit_book(id):
     Edit a book
     """
 
-    books_instance = Book.query.get_or_404(id)
+    book_instance = Book.query.get_or_404(id)
 
     LOGGER.info("Set book variable from request")
-    try:
-        books_instance.name = request.json.get("name", books_instance.name)
-        books_instance.edition = request.json.get("edition", books_instance.edition)
-        books_instance.publication_year = request.json.get("publication_year", books_instance.publication_year)
-        books_instance.authors = request.json.get("authors", books_instance.authors)
-    except KeyError as e:
-        abort(400, f"There is no key with that value: {e}")
+    request_fields = request.get_json() if request.get_json() else request.form
 
-    LOGGER.info(f"Edit book {books_instance.id} in the database")
+    book_instance.name = request_fields.get("name", book_instance.name)
+    book_instance.edition = request_fields.get("edition", book_instance.edition)
+    book_instance.publication_year = request_fields.get("publication_year", book_instance.publication_year)
+
+    if 'authors' in request_fields:
+        LOGGER.info(f"Edit authors for the book '{book_instance.name}'")
+        for author in request_fields.get("authors"):
+            author_book_instance = AuthorBook()
+            author_book_instance.author_id = author
+            author_book_instance.book_id = book_instance.id
+            try:
+                db.session.add(author_book_instance)
+                db.session.commit()
+            except SQLAlchemyError as e:
+                db.session.rollback()
+                abort(403, f"SQLAlchemyError: {e}")
+            except Exception as e:
+                abort(500, e)
+
+    LOGGER.info(f"Edit book {book_instance.id} in the database")
     try:
         # Edit book in the database
         db.session.commit()
@@ -156,7 +195,16 @@ def edit_book(id):
     except Exception as e:
         abort(500, e)
 
-    return book_schema.jsonify(books_instance), 200
+    LOGGER.info(f"Return book edited: '{book_instance.name}'")
+    author_instance = AuthorBook.query.filter_by(book_id=book_instance.id).all()
+    authors = [author.author_id for author in author_instance]
+
+    return {
+        'id': book_instance.id,
+        'name': book_instance.name,
+        'publication_year': book_instance.publication_year,
+        'authors': authors
+    }, 200
 
 
 @book.route("/books/delete/<int:id>", methods=["DELETE"])
--- src/models.py
+++ src/models.py
@@ -3,12 +3,17 @@ from sqlalchemy.orm import relationship
 
 from src import db, ma
 
-association_table = db.Table(
-    'association',
-    db.metadata,
-    db.Column('book_id', db.Integer, db.ForeignKey('books.id')),
-    db.Column('author_id', db.Integer, db.ForeignKey('authors.id'))
-)
+
+class AuthorBook(db.Model):
+    """
+    Association table between author and book (many to many relationship)
+    """
+
+    __tablename__ = 'author_books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
 
 
 class Author(db.Model):
@@ -20,7 +25,6 @@ class Author(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), index=True)
-    # books = relationship('Book', secondary=association_table, backref='authors')
 
     def get_url(self):
         return url_for("author.list_authors", id=self.id, _external=True)
@@ -34,7 +38,7 @@ class AuthorSchema(ma.SQLAlchemyAutoSchema):
         # Fields to expose
         fields = ("id", "name", "books")
         model = Author
-        load_instance = True
+        # load_instance = True
 
 
 author_schema = AuthorSchema()
@@ -52,7 +56,7 @@ class Book(db.Model):
     name = db.Column(db.String(60), index=True, nullable=False)
     edition = db.Column(db.String(10), index=True, nullable=False)
     publication_year = db.Column(db.Integer(), index=True, nullable=False)
-    authors = relationship('Author', secondary=association_table, backref='books')
+    # authors = db.relationship('Author', secondary=association_table, backref='books', lazy="joined")
 
     def get_url(self):
         return url_for("book.list_books", id=self.id, _external=True)
@@ -62,11 +66,15 @@ class Book(db.Model):
 
 
 class BookSchema(ma.SQLAlchemyAutoSchema):
+    # authors = ma.Nested(AuthorSchema, many=True)
+
     class Meta:
         # Fields to expose
         fields = ("id", "name", "edition", "publication_year", "authors")
         model = Book
-        load_instance = True
+        # load_instance = True
+        include_fk = True
+    author = ma.Nested(AuthorSchema)
 
 
 book_schema = BookSchema()
